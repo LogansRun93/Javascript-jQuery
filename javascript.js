0659aaae5b76d3ab41da6db0fc7332764a7646e5
Erik L. McGuire <erikmcguire@gmail.com>
2015-03-14 01:00:21 -0500

    Different modes implmented (random color, gradient, trail)

--- javascript.js
+++ javascript.js
@@ -1,17 +1,67 @@
+currentMode = "Default";
+
 $(document).ready(function () {
 	
 	// Create the default grid
 	generateGrid(256);
 	
-	// On the mouseover of a square, color it blue
+	// On the mouseover of a square, color the square based on the current mode
 	$("#container").on("mouseover", ".square", function() {
-		$(this).css("background-color", "blue");
+		switch(currentMode) {
+			case "Default":
+				$(this).css("background-color", "white");
+				break;
+			case "Random":
+				$(this).css("background-color", "#" + (Math.random() * 0xFFFFFF << 0).toString(16));
+				break;
+			case "Gradient":
+				currentOpacity = $(this).css("opacity");
+				if (currentOpacity > 0) {
+					$(this).css("opacity", currentOpacity - 0.1);
+				}
+				break;
+			case "Trail":
+				$(this).fadeTo(100, 0);
+				$(this).on("mouseleave", function () {
+					$(this).fadeTo(400, 1);
+				});
+				break;
+		}
 	});
 	
 	// Grid Size button was clicked
 	$( "#cmdGridSize" ).click(function() {
 	  numBlocks = Math.pow(prompt( "Enter the grid side length." ), 2);
 	  generateGrid(numBlocks);
+	});	
+	
+	// Clear grid button was clicked
+	$( "#cmdClearGrid" ).click(function() {
+		clearGrid();
+	});	
+	
+	// Default Mode button was clicked
+	$( "#cmdDefaultMode" ).click(function() {
+		currentMode = "Default";
+		clearGrid();
+	});	
+	
+	// Random Color button was clicked
+	$( "#cmdRandomMode" ).click(function() {
+		currentMode = "Random";
+		clearGrid();
+	});	
+	
+	// Gradient Mode button was clicked
+	$( "#cmdGradientMode" ).click(function() {
+		currentMode = "Gradient";
+		clearGrid();
+	});	
+	
+	// Trail Mode button was clicked
+	$( "#cmdTrailMode" ).click(function() {
+		currentMode = "Trail";
+		clearGrid();
 	});	
 });
 
@@ -36,4 +86,10 @@ function generateGrid(numBlocks) {
 	$(".square").width(squareSideLength);
 	$(".square").height(squareSideLength);
 	
+}
+
+function clearGrid() {
+	// Set all squares back to normal
+	$(".square").css("background-color", "black");
+	$(".square").css("opacity", "1");	
 }
